4911052329379ddc6a3f98333fde6299db53ca04
Michael Ripa <m.ripa123@gmail.com>
2024-08-14 12:05:33 -0400

    Moved library to environment.yml

--- services/api/start.sh
+++ services/api/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# TODO: Figure out how to get this to install from environment.yml (currently fails to)
-pip install prometheus-fastapi-instrumentator
-
 gunicorn src.app:app --bind 0.0.0.0:80 --workers $WORKERS --worker-class uvicorn.workers.UvicornWorker --timeout 120
+
