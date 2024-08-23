ac5282bb621e191adcc447fa8974fbe15ffc7706
Jaden Fiotto-Kaufman <jadenfk@outlook.com>
2024-08-23 10:44:00 -0400

    Dtensors are not converted to real local tensors upon entering the intervention graph, and are converted back into Dtensors on the way out.

--- ray/distributed/util.py
+++ ray/distributed/util.py
@@ -4,7 +4,7 @@ from typing import Any
 
 import torch
 from safetensors.torch import load_file
-from torch.distributed._tensor import DTensor, Replicate, distribute_tensor
+from torch.distributed._tensor import DTensor, Replicate
 from tqdm import tqdm
 from transformers.utils.hub import cached_file
 
@@ -26,13 +26,11 @@ def load_hf_model_from_cache(model: torch.nn.Module, repo_id: str):
 
     for shard_file in pbar:
         # Get path to shard
-        shard_path = cached_file(
-            repo_id, shard_file
-        )
+        shard_path = cached_file(repo_id, shard_file)
         pbar.set_postfix({"Current shard": shard_file})
 
         # Get path to shard
-        state_dict = load_file(shard_path, device="cpu")
+        state_dict = load_file(shard_path, device="cuda")
 
         model.load_state_dict(state_dict, strict=False, assign=True)
 
@@ -56,16 +54,15 @@ def patch_intervention_protocol() -> None:
 
                     placements.append((tensor.placements, tensor.device_mesh))
 
-                    return tensor.redistribute(
-                        placements=[Replicate()] * tensor.device_mesh.ndim,
-                        async_op=False,
-                    )
+                    return tensor.to_local()
 
                 placements.append(None)
 
                 return tensor
 
-            activations = util.apply(activations, check_for_dtensor, torch.Tensor)
+            activations = util.apply(
+                activations, check_for_dtensor, torch.Tensor
+            )
 
             activations = intervene(activations, *args, **kwargs)
 
@@ -78,14 +75,8 @@ def patch_intervention_protocol() -> None:
                 if placement is None:
 
                     return tensor
-                
+
                 placement, device_mesh = placement
-
-                if isinstance(tensor, DTensor):
-
-                    return tensor.redistribute(
-                        placements=placement, async_op=False
-                    )
 
                 return DTensor.from_local(
                     tensor, device_mesh=device_mesh, placements=placement
