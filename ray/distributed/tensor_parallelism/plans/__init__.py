f9af2768a4958518ced0fa3c8fc8433bfec6188a
Jaden Fiotto-Kaufman <jadenfk@outlook.com>
2024-08-23 11:04:12 -0400

    Add 405b base to TP model plans

--- ray/distributed/tensor_parallelism/plans/__init__.py
+++ ray/distributed/tensor_parallelism/plans/__init__.py
@@ -5,6 +5,7 @@ model_id_to_plans = {
     "meta-llama/Meta-Llama-3-70B": model_plans,
     "meta-llama/Meta-Llama-3.1-70B": model_plans,
     "meta-llama/Meta-Llama-3.1-8B": model_plans,
+    "meta-llama/Meta-Llama-3.1-405B": model_plans,
     "meta-llama/Meta-Llama-3.1-405B-Instruct": model_plans,
     "meta-llama/Meta-Llama-3.1-70B-Instruct": model_plans,
 }
