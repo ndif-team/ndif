6751be6b38fb6dc0abe021fdb9e95e0d76593e3a
jadenfk@outlook.com <jadenfk@outlook.com>
2024-09-02 17:43:15 -0400

    Merge

--- ray/resources.py
+++ ray/resources.py
@@ -2,16 +2,21 @@ import json
 
 from .util import get_total_cudamemory_MBs
 
-def main(head: bool):
+
+def main(head: bool, name: str = None):
 
     resources = {}
 
     if head:
 
-        resources["head"] = 1
+        resources["head"] = 10
 
     resources["cuda_memory_MB"] = get_total_cudamemory_MBs()
-        
+
+    if name is not None:
+
+        resources[name] = 10
+
     print(json.dumps(resources))
 
 
@@ -20,5 +25,5 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--head", action="store_true")
-
+    parser.add_argument("--name", default=None)
     main(**vars(parser.parse_args()))
