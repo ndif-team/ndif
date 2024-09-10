39375f8b46abb48df1d704fd2e90d2e1ecb1dca8
Michael Ripa <m.ripa123@gmail.com>
2024-09-10 15:55:56 -0400

    Added deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Script Name: deploy.sh
+# Description: This script deploys (or redeploys) Ray model deployments on a list of specified machines.
+# Author: Michael Ripa
+# Date: 2024-09-10
+
+##################### CONFIG ######################################################################
+
+MACHINES=("hamada" "fukuyama") # List of machines/ip's (for ssh)
+USER="ndif" # Username (for ssh)
+REPOS="ndif nnsight"  # Git repos to pull updates from
+START_SCRIPT_DIR="~/ndif-deployment" # Path to directory containing env.sh, start.sh and download.py
+CONDA_ENV="prod" # Name of conda environment
+
+##################### SCRIPT #######################################################################
+
+for MACHINE in "${MACHINES[@]}"; do
+  echo "Connecting to $MACHINE as $USER..."
+  
+  # Run the following commands on the remote machine
+  ssh $USER@$MACHINE << EOF
+    echo "Attaching to tmux session on $MACHINE..."
+   
+    # Attach to the most recent tmux session (or create new session if none found)
+    TMUX_SESSION=\$(tmux ls | tail -n 1 | awk -F: '{print \$1}')
+    if [ -z "\$TMUX_SESSION" ]; then
+      echo "No tmux session found on $MACHINE. Starting new tmux session..."
+      tmux
+    else
+      tmux attach-session -t \$TMUX_SESSION
+    fi
+
+    # Stop the running Ray node (press Ctrl+C)
+    echo "Stopping current Ray node..."
+    tmux send-keys -t \$TMUX_SESSION C-c
+
+    # Ensure all Python scripts are killed (probably optional)
+    echo "Killing all Python scripts..."
+    pkill -u $USER -f python  # Kills all Python processes
+
+    cd $START_SCRIPT_DIR
+    source ~/miniconda3/etc/profile.d/conda.sh
+
+    # Activate the specific conda environment
+    conda activate $CONDA_ENV
+
+    # Navigate to each repo, pull updates
+    START_SCRIPT_DIR="$START_SCRIPT_DIR"
+    REPOS="$REPOS"
+    for REPO in \$REPOS; do
+      echo "Updating repository: \$REPO..."
+      cd \$REPO
+      git pull
+      cd ../
+    done
+
+    # Start the node
+    cd \$START_SCRIPT_DIR
+    bash start.sh
+
+    # Detach from tmux session and disconnect
+    tmux detach
+    echo "Tasks completed on $MACHINE. Disconnecting..."
+
+EOF
+done
+
+echo "All tasks completed."
