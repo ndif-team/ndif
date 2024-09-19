9878233b5ed22e8a1193492379be9f9887d37187
Michael Ripa <m.ripa123@gmail.com>
2024-09-19 15:43:03 -0400

    Made changes so that ray-head-start.sh is identical for prod and dev

--- compose/prod/ray-head-start.sh
+++ compose/prod/ray-head-start.sh
@@ -4,13 +4,13 @@ resources=`python -m src.ray.resources --head`
 
 ray start --head \
     --resources="$resources" \
-    --port=6379 \
-    --object-manager-port=8076 \
+    --port=$RAY_HEAD_INTERNAL_PORT \
+    --object-manager-port=$OBJECT_MANAGER_PORT \
     --include-dashboard=true \
-    --dashboard-host=0.0.0.0 \
-    --dashboard-port=8265 \
-    --dashboard-grpc-port=8268 \
-    --metrics-export-port=8267
+    --dashboard-host=$RAY_DASHBOARD_HOST \
+    --dashboard-port=$RAY_DASHBOARD_INTERNAL_PORT \
+    --dashboard-grpc-port=$RAY_DASHBOARD_GRPC_PORT \
+    --metrics-export-port=$RAY_SERVE_INTERNAL_PORT
     
 serve deploy src/ray/config/ray_config.yml
 
