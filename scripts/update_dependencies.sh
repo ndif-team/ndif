e6d8626e3a9d6eb4676ef433e927e054b10bb259
Michael Ripa <m.ripa123@gmail.com>
2024-10-02 16:55:01 -0400

    Added docstring and debug mode

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+# This script is for live updates of NDIF service environments.
+# It allows you to make changes to any service's environment.yml
+# without needing to rebuild from base.
+#
+# Usage: ./update_dependencies.sh <environment>
+#
+# This script can be run automatically for all services by calling `make update`
+#
+# Debug mode:
+# Set the DEBUG environment variable to 'true' to enable debug output
+# Example: DEBUG=true ./update_dependencies.sh <environment>
+#
+# This script will:
+# 1. Copy the updated environment.yml to each running service container
+# 2. Update the conda environment in each container
+# 3. Restart all services to apply the changes
+
 # Check if environment argument is provided
 if [ $# -eq 0 ]; then
     echo "No environment specified. Usage: $0 <environment>"
     exit 1
 fi
 
+DEBUG=${DEBUG:-false} # Set debug mode to false if not set
 ENV=$1
 COMPOSE_FILE="compose/$ENV/docker-compose.yml"
 
@@ -22,21 +40,27 @@ export N_DEVICES=$(nvidia-smi -L | wc -l)
 # Array of services to update
 services=("api" "ray-head" "ray-worker")
 
+
+
 for service in "${services[@]}"; do
     echo "Updating $service..."
     
-    echo "Trying to find container for service: $service"
-    echo "Using compose file: $COMPOSE_FILE"
-    
-    # List all containers
-    echo "All containers:"
-    docker compose -f $COMPOSE_FILE ps
+    if [ "$DEBUG" = true ]; then
+        echo "Trying to find container for service: $service"
+        echo "Using compose file: $COMPOSE_FILE"
+        
+        # List all containers
+        echo "All containers:"
+        docker compose -f $COMPOSE_FILE ps
+    fi
     
     # Try to get the container ID
     container_id=$(docker compose -f $COMPOSE_FILE ps -q $service)
     
-    echo "Command used: docker compose -f $COMPOSE_FILE ps -q $service"
-    echo "Result (container_id): '$container_id'"
+    if [ "$DEBUG" = true ]; then
+        echo "Command used: docker compose -f $COMPOSE_FILE ps -q $service"
+        echo "Result (container_id): '$container_id'"
+    fi
     
     if [ -z "$container_id" ]; then
         echo "Container for $service is not running or not found. Skipping..."
@@ -48,7 +72,9 @@ for service in "${services[@]}"; do
     service_path=${service//-/_}
 
     docker cp services/$service_path/environment.yml $container_id:/tmp/environment.yml
-    echo "Copied environment.yml to container"
+    if [ "$DEBUG" = true ]; then
+        echo "Copied environment.yml to container"
+    fi
 
     # Update the conda environment
     docker exec -it $container_id /bin/bash -c \
