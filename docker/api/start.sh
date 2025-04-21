cd13f1fb0cb9fb7afe4a47e437109f5da3f71eca
Michael Ripa <m.ripa123@gmail.com>
2025-04-21 13:06:12 -0400

    Fix 'make ta' to apply updates from source-code

--- docker/api/start.sh
+++ docker/api/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Install the volume-mounted packages (to avoid rebuilding the image for every source code change)
+pip install -e /ndif-core
+pip install -e /ndif-api
+
+
 gunicorn ndif.api.app:app --bind 0.0.0.0:80 --workers $WORKERS --worker-class uvicorn.workers.UvicornWorker --timeout 120
 
--- docker/ray/start.sh
+++ docker/ray/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install the volume-mounted packages (to avoid rebuilding the image for every source code change)
+pip install -e /ndif-core
+pip install -e /ndif-ray
+
+# Obtain the resources required for the Ray cluster
 resources=`python -m ndif.ray.resources --head`
 
 # Start Ray with environment variables from env_file
@@ -13,6 +18,7 @@ ray start --head \
     --dashboard-grpc-port=$RAY_DASHBOARD_GRPC_PORT \
     --metrics-export-port=$RAY_SERVE_INTERNAL_PORT
  
+# Deploy the Ray cluster
 serve deploy ndif-ray/src/ndif/ray/config/ray_config.yml
 
 tail -f /dev/null
