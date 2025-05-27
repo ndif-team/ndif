4ed64a6e75cda3d73466be1d90698348fc839418
Michael Ripa <51883134+MichaelRipa@users.noreply.github.com>
2025-05-27 13:02:54 -0400

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,12 +1,12 @@
 import nnsight
 
-nnsight.CONFIG.set_default_api_key("03dd83e1-56d4-4f96-9c13-a7ecb991c210")
+nnsight.CONFIG.set_default_api_key("api key")
 nnsight.CONFIG.API.HOST = "localhost:5001"
 nnsight.CONFIG.API.SSL = False
 
 model = nnsight.LanguageModel("openai-community/gpt2")
 
-with model.trace("NDIF on AWS is really ", remote=True):
+with model.trace("The Eiffel Tower is located in ", remote=True):
     
     output = model.output.save()
     
