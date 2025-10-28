737e2d709e4830b8c1c910657b6d2443383f3465
michaelwaves <michaelyu713705@gmail.com>
2025-10-28 12:24:26 -0700

    add test_ping
    test_app.py tests ping api route
    note: fastapi TestClient(app) doesn't work due to fastapi mounting socketio
    you must cd to the src/services/api directory before running pytest (added this to readme)
    this is currently in a draft state and should not be merged

---
+++ src/services/api/tests/test_app.py
@@ -0,0 +1,10 @@
+import pytest
+from httpx import AsyncClient, ASGITransport
+from src.app import app
+
+@pytest.mark.asyncio
+async def test_ping():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+        response = await client.get("/ping")
+        assert response.status_code == 200
+        assert response.json() == "pong"
