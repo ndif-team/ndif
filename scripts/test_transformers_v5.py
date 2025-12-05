04e7595462c02bb32a36f0c86e03f2aa39453725
Avery Yen <haplesshero13@kyoto>
2025-12-05 15:19:27 -0500

    Revert "try manager"
    This reverts commit a9328e4ac04ff1c4535d8a8405f6885f62e08e05.

--- scripts/test_transformers_v5.py
+++ scripts/test_transformers_v5.py
@@ -24,23 +24,19 @@ tokenized_datasets = dataset.map(lambda x: tokenizer(x["question"]), batched=Tru
 simple_batch_inputs = [item["input_ids"] for item in tokenized_datasets]
 
 generation_config = GenerationConfig(
-    max_new_tokens=128,
+    max_new_tokens=32,
+    use_cuda_graph=False,  # Not supported for simple version
     eos_token_id=tokenizer.eos_token_id,
     pad_token_id=tokenizer.pad_token_id,
     do_sample=False,
     max_batch_tokens=512,  # max number of tokens in a batch, this is just a default value you should tune based on your hardware
 )
 
-manager = model.init_continuous_batching(generation_config=generation_config)
+batch_outputs = model.generate_batch(
+    inputs=simple_batch_inputs,
+    generation_config=generation_config,
+)
 
-# start the background thread
-manager.start()
-
-# this is for demonstration purposes only, in practice this is most useful to do concurrently
-for i, input in enumerate(simple_batch_inputs):
-    request_id = manager.add_request(input_ids=input, request_id=f"request_{i}")  # if you do not specify a request_id, one will be generated for you
-
-# Can be done in another thread
-for id, request in manager.get_result():
-    generated_text = tokenizer.decode(request.generated_tokens, skip_special_tokens=True)
-    print(f"Request {id} output: {generated_text}")
+for request_id, output in batch_outputs.items():
+    generated_text = tokenizer.decode(output.generated_tokens, skip_special_tokens=True)
+    print(f"Request {request_id} output: {generated_text}")
