997eb58017ca488b2cf90c2a082118ff3978268a
Michael <michaelyu713705@gmail.com>
2025-12-21 01:49:04 +0000

    add 3 mailgun tests

--- src/common/tests/unit/test_mailgun_provider.py
+++ src/common/tests/unit/test_mailgun_provider.py
@@ -1,5 +1,4 @@
 from common.providers.mailgun import MailgunProvider
-import os
 import pytest
 
 
@@ -9,7 +8,7 @@ def reset_mailgun_provider():
     MailgunProvider.domain = None
 
 
-def test_mailgun_provider(monkeypatch):
+def test_mailgun_provider_from_env(monkeypatch):
     monkeypatch.setenv("MAILGUN_DOMAIN", "nnsight.net")
     monkeypatch.setenv("MAILGUN_API_KEY", "sk-123")
 
@@ -17,3 +16,25 @@ def test_mailgun_provider(monkeypatch):
 
     assert MailgunProvider.domain == "nnsight.net"
     assert MailgunProvider.api_key == "sk-123"
+
+
+def test_mailgun_provider_to_env(monkeypatch):
+    monkeypatch.setenv("MAILGUN_DOMAIN", "nnsight.net")
+    monkeypatch.setenv("MAILGUN_API_KEY", "sk-123")
+
+    MailgunProvider.from_env()
+
+    result = MailgunProvider.to_env()
+    assert result["MAILGUN_DOMAIN"] == "nnsight.net"
+    assert result["MAILGUN_API_KEY"] == "sk-123"
+
+
+def test_mailgun_provider_connected_success(monkeypatch):
+    monkeypatch.setenv("MAILGUN_DOMAIN", "nnsight.net")
+    monkeypatch.setenv("MAILGUN_API_KEY", "sk-123")
+    MailgunProvider.from_env()
+    assert MailgunProvider.connected()
+
+
+def test_mailgun_provider_connected_fail():
+    assert not MailgunProvider.connected()
