2f73558057aca0899977d9c94694a10071cab7f8
Michael Ripa <m.ripa123@gmail.com>
2026-03-02 14:56:31 -0500

    bug fix for multi worker

--- src/services/ray/start-worker.sh
+++ src/services/ray/start-worker.sh
@@ -11,12 +11,19 @@ fi
 RAY_TEMP_DIR="${NDIF_RAY_TEMP_DIR:-/tmp/ray}"
 mkdir -p "$RAY_TEMP_DIR"
 
+# Convert ray:// address to IP:PORT format for ray start
+# NDIF_RAY_ADDRESS may be "ray://IP:PORT" or "IP:PORT"
+# ray start --address needs "IP:HEAD_PORT"
+RAY_ADDR_CLEAN=$(echo "${NDIF_RAY_ADDRESS}" | sed 's|ray://||')
+RAY_HEAD_IP=$(echo "${RAY_ADDR_CLEAN}" | cut -d':' -f1)
+RAY_GCS_ADDRESS="${RAY_HEAD_IP}:${NDIF_RAY_HEAD_PORT}"
+
 echo "Starting Ray worker node..."
-echo "  Connecting to: ${NDIF_RAY_ADDRESS}"
+echo "  Connecting to: ${RAY_GCS_ADDRESS}"
 echo "  Temp dir: $RAY_TEMP_DIR"
 
 ray start --resources "$resources" \
-    --address ${NDIF_RAY_ADDRESS} \
+    --address "${RAY_GCS_ADDRESS}" \
     --temp-dir="$RAY_TEMP_DIR"
 
 if [ $? -ne 0 ]; then
