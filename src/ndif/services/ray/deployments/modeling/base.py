00b67b9973f577eec2bfa803536eb8782c102091
Michael Ripa <m.ripa123@gmail.com>
2026-08-03 14:48:30 -0400

    remove accidentally added peft functionality

--- src/ndif/services/ray/deployments/modeling/base.py
+++ src/ndif/services/ray/deployments/modeling/base.py
@@ -421,8 +421,6 @@ class BaseModelDeployment:
                 description="Your job has started running.",
             )
 
-            span.add_event("applying_env")
-            self.model._remoteable_set_env(self.request.env)
             self.persistent_objects = self.model._remoteable_persistent_objects()
 
             span.add_event("deserializing_request")
